54e741d394d3673799e89b49bac2a2299cc0914d
Blackjoker52 <zishankadri9@gmail.com>
2024-06-14 16:40:32 +0530

    update

--- project/tailwind.config.js
+++ project/tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
     './*/templates/**/*.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand': '#DEAAF2',
+        'brand-hv': '#EFC0F9',
+        'brand-light':'#F9DFFF',
+        'brand-dark': '#201A34',
+        'brand-dark-xl': '#141020',
+      },
+      fontFamily: {
+        'poppins': ['Poppins'],
+        'handlee': ['Handlee'],
+        'pangolin': ['Pangolin'],
+     }
+    },
   },
   plugins: [],
 }
